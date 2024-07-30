3c75c5fc93b08f6bbbcb0586ecfdf328895ec7cf
emerson <emerson.soares.sc@gmail.com>
2024-07-29 22:24:22 -0300

    atualizando curva abc

--- estoque.py
+++ estoque.py
@@ -1,15 +1,13 @@
 import streamlit as st
+import pandas as pd
 from function import analise_estoque, analise_estoque_grupo
 
 
 def layout_estoque():
     filial = st.selectbox('Escolha a filial:', ['001 - Matriz','004 - Centrinho','005 - Calil','007 - Rio Vermelho','008 - Vargem','009 - Canasvieiras','010 - Upa','011 - Trindade','100 - Central'])
     cod_filial = filial[0:3]
-    st.write(cod_filial)
-    
     
     valor_em_estoque, valor_faltas, df_estoque = analise_estoque(cod_filial)
-    
     
     valor_estoque_base = valor_em_estoque+valor_faltas
     
@@ -69,10 +67,24 @@ def layout_estoque():
         st.metric(label='R$ faltas do grupo', value=f'R$ {(faltas_grupo/1000):.1f} mil')
         
     st.write(df_faltas_grupo)
+
+    '''
+    st.header('Indicador de Excesso de estoque')
+    df_excesso_grupo['preco_custo'] = pd.to_numeric(df_excesso_grupo['preco_custo'], errors='coerce')
+    df_excesso_grupo['valor_excesso'] = df_excesso_grupo['preco_custo']*df_excesso_grupo['excesso']
+    excesso = df_excesso_grupo['valor_excesso'].sum()
+    itens_excesso = df_excesso_grupo['excesso'].sum()
     
+    kpi1,kpi2,kpi3 =  st.columns(3)
+    with kpi1:
+        st.metric(label='% de excesso no grupo', value=f'{(excesso/estoque_base_grupo*100):.1f}%')
+    with kpi2:
+        st.metric(label='R$ excesso do grupo', value=f'R$ {(excesso/1000):.1f} mil')
+    with kpi3:
+        st.metric(label='Qtd itens em excesso do grupo', value=f'{itens_excesso}')
     st.subheader('Produtos com estoque em excesso (estoque > demanda)')
     st.write(df_excesso_grupo)
-    
+    '''
     
     st.subheader('Vendas Perdidas')
     st.warning('Ainda estamos trabalhando nisso! Aguarde!')
