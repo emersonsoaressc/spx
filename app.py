4c0c07db827d6063a1fd632b7f4b6d2617a0fbf4
emerson <emerson.soares.sc@gmail.com>
2024-08-04 12:13:45 -0300

    .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from estoque import layout_estoque
 st.sidebar.image('images/logo_shopfarma_sem_fundo.png')
 
 with st.sidebar.expander('Gestão de estoque avançada', expanded=True):
-    gestao_lst = ['Home','Sistema de gestao']
+    gestao_lst = ['Home','Sistema de compras']
     gestao_selectbox = st.selectbox('',gestao_lst)
 
 if gestao_selectbox == 'Home':
