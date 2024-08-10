16944fecdde1794d1ccad13d9ddafb59b2fb69fc
emerson <emerson.soares.sc@gmail.com>
2024-08-09 21:32:42 -0300

    .

--- home.py
+++ home.py
@@ -18,7 +18,7 @@ def home():
     #001 - Matriz
     
     with st.expander('001 - Matriz', expanded=True):
-        valor_em_estoque_001, valor_faltas_001, df_estoque_001 = analise_estoque('001')
+        valor_em_estoque_001, valor_faltas_001, df_estoque_001 = analise_estoque('001','xls')
         valor_estoque_base_001 = valor_em_estoque_001+valor_faltas_001
         kpi1,kpi2,kpi3 =  st.columns(3)
         with kpi1:
@@ -30,7 +30,7 @@ def home():
         
     #004 - Centrinho
     with st.expander('004 - Centrinho', expanded=True):
-        valor_em_estoque_004, valor_faltas_004, df_estoque_004 = analise_estoque('004')
+        valor_em_estoque_004, valor_faltas_004, df_estoque_004 = analise_estoque('004','xls')
         valor_estoque_base_004 = valor_em_estoque_004+valor_faltas_004
         kpi1,kpi2,kpi3 =  st.columns(3)
         with kpi1:
@@ -42,7 +42,7 @@ def home():
             
     #005 - Calil
     with st.expander('005 - Calil', expanded=True):
-        valor_em_estoque_005, valor_faltas_005, df_estoque_005 = analise_estoque('005')
+        valor_em_estoque_005, valor_faltas_005, df_estoque_005 = analise_estoque('005','xls')
         valor_estoque_base_005 = valor_em_estoque_005+valor_faltas_005
         kpi1,kpi2,kpi3 =  st.columns(3)
         with kpi1:
@@ -54,7 +54,7 @@ def home():
 
     #007 - Rio Vermelho
     with st.expander('007 - Rio Vermelho', expanded=True):
-        valor_em_estoque_007, valor_faltas_007, df_estoque_007 = analise_estoque('007')
+        valor_em_estoque_007, valor_faltas_007, df_estoque_007 = analise_estoque('007','xls')
         valor_estoque_base_007 = valor_em_estoque_007+valor_faltas_007
         kpi1,kpi2,kpi3 =  st.columns(3)
         with kpi1:
@@ -66,7 +66,7 @@ def home():
             
     #008 - Vargem
     with st.expander('008 - Vargem', expanded=True):
-        valor_em_estoque_008, valor_faltas_008, df_estoque_008 = analise_estoque('008')
+        valor_em_estoque_008, valor_faltas_008, df_estoque_008 = analise_estoque('008','xls')
         valor_estoque_base_008 = valor_em_estoque_008+valor_faltas_008
         kpi1,kpi2,kpi3 =  st.columns(3)
         with kpi1:
@@ -78,7 +78,7 @@ def home():
 
     #009 - Canasvieiras
     with st.expander('009 - Canasvieiras', expanded=True):
-        valor_em_estoque_009, valor_faltas_009, df_estoque_009 = analise_estoque('009')
+        valor_em_estoque_009, valor_faltas_009, df_estoque_009 = analise_estoque('009','xls')
         valor_estoque_base_009 = valor_em_estoque_009+valor_faltas_009
         kpi1,kpi2,kpi3 =  st.columns(3)
         with kpi1:
@@ -90,7 +90,7 @@ def home():
             
     #010 - Upa Norte
     with st.expander('010 - Upa Norte', expanded=True):
-        valor_em_estoque_010, valor_faltas_010, df_estoque_010 = analise_estoque('010')
+        valor_em_estoque_010, valor_faltas_010, df_estoque_010 = analise_estoque('010','xls')
         valor_estoque_base_010 = valor_em_estoque_010+valor_faltas_010
         kpi1,kpi2,kpi3 =  st.columns(3)
         with kpi1:
@@ -102,7 +102,7 @@ def home():
             
     #011 - Trindade
     with st.expander('011 - Trindade', expanded=True):
-        valor_em_estoque_011, valor_faltas_011, df_estoque_011 = analise_estoque('011')
+        valor_em_estoque_011, valor_faltas_011, df_estoque_011 = analise_estoque('011','xls')
         valor_estoque_base_011 = valor_em_estoque_011+valor_faltas_011
         kpi1,kpi2,kpi3 =  st.columns(3)
         with kpi1:
