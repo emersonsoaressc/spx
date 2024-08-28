042261143a247945098ffa1f389d2bb8254af350
emerson <emerson.soares.sc@gmail.com>
2024-08-28 12:19:02 -0300

    .

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import streamlit as st
 from compras.compras import layout_compras
 from home import home
-
+from colaboradores import dash_colab
 
 st.sidebar.image('images/logo_shopfarma_sem_fundo.png')
 
-with st.sidebar.expander('Gestão de estoque avançada', expanded=True):
-    gestao_lst = ['Home','Sistema de compras']
-    gestao_selectbox = st.selectbox('',gestao_lst)
+with st.sidebar.expander('Gestão de colaboradores',expanded=True):
+    lst_gestao_colab = ['','Dashboard Geral','Avaliação Individual']
 
-if gestao_selectbox == 'Home':
+    if lst_gestao_colab == 'Dashboard Geral':
+        dash_colab()
+
+with st.sidebar.expander('Gestão de estoque avançada', expanded=False):
+    lst_gestao_estoque = ['','Home','Sistema de compras']
+    gestao_estoque_selectbox = st.selectbox('',lst_gestao_estoque)
+
+if gestao_estoque_selectbox == 'Home':
     home()
 
-if gestao_selectbox == 'Dashboard':
+if gestao_estoque_selectbox == 'Dashboard':
     pass
     
-if gestao_selectbox == 'Sistema de compras':
+if gestao_estoque_selectbox == 'Sistema de compras':
     layout_compras()
---
+++ colaboradores.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from function import analise_estoque, analise_estoque_grupo
+
+def dash_colab():
+    st.write('Dashboard Geral')
