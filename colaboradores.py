65106774d90f9b88b8a137077b867db205e54a09
emerson <emerson.soares.sc@gmail.com>
2024-08-29 09:07:35 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -7,8 +7,23 @@ def dash_colab():
     st.subheader('Colaboradores')
     kpi1,kpi2,kpi3 =  st.columns(3)
     with kpi1:
-        st.metric(label='Colaboradores ativos', value=f'')
+        st.metric(label='Colaboradores ativos', value=0, delta=0.1)
     with kpi2:
-        st.metric(label='Venda por colaborador', value=f'')
+        st.metric(label='Venda por colaborador', value=0, delta=0.1)
     with kpi3:
-        st.metric(label='Lucro Bruto por colaborador', value=f'')
+        st.metric(label='Lucro Bruto por colaborador', value=0, delta=0.1)
+    
+    # melhores vendedores em faturamento
+    st.subheader('Melhores Vendedores em faturamento')
+    
+    
+    # melhores vendedores em lucratividade bruta
+    st.subheader('Melhores Vendedores em lucratividade bruta')
+    
+    
+    # melhores vendedores em ticket médio
+    st.subheader('Melhores Vendedores em ticket médio')
+    
+    
+    # melhores vendedores em itens por cupom
+    st.subheader('Melhores Vendedores em ticket médio')
