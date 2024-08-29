ccbd354ac3f70b250f7cdfa82bd4d152acb878ac
emerson <emerson.soares.sc@gmail.com>
2024-08-29 09:03:33 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -4,11 +4,11 @@ from function import analise_estoque, analise_estoque_grupo
 def dash_colab():
     st.header('Dashboard Geral')
     
-    st.subheader('Indicadores de Faturamento')
+    st.subheader('Colaboradores')
     kpi1,kpi2,kpi3 =  st.columns(3)
     with kpi1:
-        st.metric(label='Faturamento Atual', value=f'')
+        st.metric(label='Colaboradores ativos', value=f'')
     with kpi2:
-        st.metric(label='Faturamento ', value=f'')
+        st.metric(label='Venda por colaborador', value=f'')
     with kpi3:
-        st.metric(label='Faturamento', value=f'')
+        st.metric(label='Lucro Bruto por colaborador', value=f'')
