48c5b2eec42ab27eadbae8165b613f2e2d0021b8
emerson <emerson.soares.sc@gmail.com>
2024-08-29 08:52:33 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -2,4 +2,13 @@ import streamlit as st
 from function import analise_estoque, analise_estoque_grupo
 
 def dash_colab():
-    st.subheader('Dashboard Geral')
+    st.header('Dashboard Geral')
+    
+    st.subheader('Indicadores de Faturamento')
+    kpi1,kpi2,kpi3 =  st.columns(3)
+    with kpi1:
+        st.metric(label='Faturamento Atual', value=f'')
+    with kpi2:
+        st.metric(label='Faturamento ', value=f'')
+    with kpi3:
+        st.metric(label='Faturamento', value=f'')
