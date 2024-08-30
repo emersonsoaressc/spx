5bf51c890d1d19cdd4c7153e15a134271ca69521
emerson <emerson.soares.sc@gmail.com>
2024-08-29 21:03:21 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -55,7 +55,7 @@ def colab_individual():
     
     # inserir tratamento de erros
 
-    df_vendedor = df_relacao_vendas.query('vendedor in @lista_codigos_vendedores')
+    df_vendedor = df_relacao_vendas.query('vendedor.str.contains("lista_codigos_vendedores")')
     st.write(df_vendedor)
     dt1,dt2 =  st.columns(2)
     with dt1:
