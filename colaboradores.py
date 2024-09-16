592ca13eb53a33866b8f7e4cdfdbff065416803b
emerson <emerson.soares.sc@gmail.com>
2024-09-16 16:27:20 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -211,7 +211,10 @@ def colab_individual():
         with col_meta_3:
             st.metric(label='Meta 2', value= f'{meta_2_icon}', help=f'Desconto abaixo de 10%. O seu desconto foi de {desconto_percent}%')
             st.metric(label='Meta 5', value= f'{meta_5_icon}', help=f'Venda total acima de 50.000,00 . Você vendeu {venda_liquida}')
-    
+    else:
+        comissao_otavio =  vendas_genericos_similares*0,05 + df_vendas_csr_gensim['valor_liquido'].sum()*0.05
+        st.success(f'Sua Comissão referente as vendas do mês {data_inicial.month} é de R$ {round(comissao_otavio)}')
+        
     if meta_zero > 0:
         st.success(f'Sua Comissão referente as vendas do mês {data_inicial.month} é de R$ {round(meta_zero+meta_1+meta_2+meta_3+meta_4+meta_5+meta_6,2)}')
     else:
