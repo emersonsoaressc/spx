8c80de70f8f544dd46ce085b3aa2db1d1accbebb
emerson <emerson.soares.sc@gmail.com>
2024-09-16 16:46:31 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -200,17 +200,17 @@ def colab_individual():
         col_meta_1,col_meta_2,col_meta_3 =  st.columns(3)
         with col_meta_1:
             st.metric(label='Meta ZERO - R$ 25.000,00', value=f'{meta_zero_icon}', help=f'Essa meta ativa as outras metas, se não atingir a meta ZERO, não tem direito as outras metas!')
-            st.metric(label='Meta 3', value= f'{meta_3_icon}', help=f'Itens por cliente acima de 2.00. O seu IPC foi de {ipc}')
-            st.metric(label='Meta 6', value= f'{meta_6_icon}', help=f'Venda total acima de 70.000,00 . Você vendeu {venda_liquida}')
+            st.metric(label='Meta 3 - IPC', value= f'{meta_3_icon}', help=f'Itens por cliente acima de 2.00. O seu IPC foi de {ipc}')
+            st.metric(label='Meta 6 - 70k', value= f'{meta_6_icon}', help=f'Venda total acima de 70.000,00 . Você vendeu {venda_liquida}')
             
             
         with col_meta_2:
-            st.metric(label='Meta 1', value= f'{meta_1_icon}', help=f'Ticket médio acima de 50,00. O seu TKM foi de {tkm}')
-            st.metric(label='Meta 4', value= f'{meta_4_icon}', help=f'Venda de Perfumaria acima de 10.000,00. Você vendeu {vendas_perfumaria}')
+            st.metric(label='Meta 1 - TKM', value= f'{meta_1_icon}', help=f'Ticket médio acima de 50,00. O seu TKM foi de {tkm}')
+            st.metric(label='Meta 4 - Perfumaria', value= f'{meta_4_icon}', help=f'Venda de Perfumaria acima de 10.000,00. Você vendeu {vendas_perfumaria}')
             
         with col_meta_3:
-            st.metric(label='Meta 2', value= f'{meta_2_icon}', help=f'Desconto abaixo de 10%. O seu desconto foi de {desconto_percent}%')
-            st.metric(label='Meta 5', value= f'{meta_5_icon}', help=f'Venda total acima de 50.000,00 . Você vendeu {venda_liquida}')
+            st.metric(label='Meta 2 - Descontos', value= f'{meta_2_icon}', help=f'Desconto abaixo de 10%. O seu desconto foi de {desconto_percent}%')
+            st.metric(label='Meta 5 - 50k', value= f'{meta_5_icon}', help=f'Venda total acima de 50.000,00 . Você vendeu {venda_liquida}')
         if meta_zero > 0:
             st.success(f'Sua Comissão referente as vendas do mês {data_inicial.month} é de R$ {round(meta_zero+meta_1+meta_2+meta_3+meta_4+meta_5+meta_6,2)}')
         else:
