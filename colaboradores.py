79f055ce62c02fd3160cd9f843ff7c4902a13ed4
emerson <emerson.soares.sc@gmail.com>
2024-09-16 15:41:49 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -163,7 +163,7 @@ def colab_individual():
         meta_2_icon = f'❌'
         
     # meta_3
-    if (meta_zero>0) and (ipc > 2.0):
+    if (meta_zero>0 and ipc > 2.0):
         meta_3 = round(100.00,2)
         meta_3_icon = f'R$ {meta_3} ✅'
     else:
