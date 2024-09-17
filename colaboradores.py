9778ae077828f3071db0cb7c1227fbe1d013c1eb
emerson <emerson.soares.sc@gmail.com>
2024-09-17 13:41:02 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -213,12 +213,12 @@ def colab_individual():
         with col_meta_2:
             st.metric(label='Meta 1 - TKM', value= f'{meta_1_icon}', help=f'Ticket médio acima de 50,00. O seu TKM foi de {tkm}')
             st.metric(label='Meta 4 - Perfumaria', value= f'{meta_4_icon}', help=f'Venda de Perfumaria acima de 10.000,00. Você vendeu {vendas_perfumaria}')
-            st.metric(label='teste bonificados 5 reais', value=0)
+            st.metric(label='Bonificados 5 reais', value=f'{bonificados_5reais}')
             
         with col_meta_3:
             st.metric(label='Meta 2 - Descontos', value= f'{meta_2_icon}', help=f'Desconto abaixo de 10%. O seu desconto foi de {desconto_percent}%')
             st.metric(label='Meta 5 - 50k', value= f'{meta_5_icon}', help=f'Venda total acima de 50.000,00 . Você vendeu {venda_liquida}')
-            
+            st.metric(label='Bonificados 10 reais', value=f'{bonificados_10reais}')
         if meta_zero > 0:
             st.success(f'Sua Comissão referente as vendas do mês {data_inicial.month} é de R$ {round(meta_zero+meta_1+meta_2+meta_3+meta_4+meta_5+meta_6,2)}')
         else:
