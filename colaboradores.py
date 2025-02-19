080156a2daed8ed93e48a95a886e1fe912129d0f
emersonsoaressc <emerson.soares.sc@gmail.com>
2025-02-19 12:41:17 -0300

    .

--- colaboradores.py
+++ colaboradores.py
@@ -234,8 +234,8 @@ def colab_individual():
         meta_total = round(bonificados_10reais+bonificados_5reais+bonificados_2_5reais+validade,2)
         st.error(f'Infelizmente você não atingiu a META ZERO no mês {data_inicial.month}. Sua comissão é de {meta_total}')
     
-    st.write(lst_vendedor[0].split()[0])
-    if (lst_vendedor[0].split()[0]) in lst_lideres:
+    st.write(seletor_colab)
+    if (seletor_colab[0]) in lst_lideres:
         st.write('Você é um LÍDER!')
 
 colab_individual()
